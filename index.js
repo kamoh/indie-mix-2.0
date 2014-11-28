f3d078e5ddc9be66539fa0757841e06fc21e0b4a
kamoh <ben.serviss@gmail.com>
2014-11-28 02:11:53 -0500

    Adds async chat & field for usernames

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+    console.log('message: ' + msg);
+  });
   console.log('a user connected');
   socket.on('disconnect', function(){
     console.log('user disconnected');
