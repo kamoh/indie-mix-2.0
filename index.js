a1c039bab23bb9bf4eae6b5d0c3523854fad1def
kamoh <ben.serviss@gmail.com>
2014-12-07 01:39:20 -0500

    Adds 'user left' message support

--- index.js
+++ index.js
@@ -16,46 +16,57 @@ io.on('connection', function(socket){
   socket.on('sign on', function(name){
     allClients.push(name);
     io.emit('someone joined', name);
+    socket.name = name;
     updateClientUserList(allClients);
     console.log(name + ' joined the chat');
     console.log('current clients: ' + allClients.toString());
     });
-  // socket.on('sync user list', function(listOfNames){
-  //   io.emit('sync user list', listOfNames);
-  //   console.log('syncing user names');
-  // });
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
     console.log('message: ' + msg);
   });
   socket.on('name change', function(defaultName, newName){
-    // io.emit('name change', defaultName, newName);
     updateServerUserList(defaultName, newName, allClients);
     console.log('roll call updated');
   });
-  socket.on('disconnect', function(name){
-    io.emit('user left', name);
-    console.log(this.name + ' disconnected');
-  });
+  socket.on('disconnect', function(){
+    console.log(socket.name + ' has discon7nected');
+    updateServerListUserLeft(socket.name, allClients);
+    io.sockets.emit('user left', socket.name);
+    console.log('clinet list after user left: ' + allClients.toString());
+  })
   
   // Functions
 
-  function updateServerUserList(oldName, newName, clientList) {
-    console.log('started server user list function');
-    console.log('client list: ' + clientList.toString());
+  function updateServerListUserLeft(name, clientList) {
     for (var i = 0; i < clientList.length; i++) {
-      console.log('in for looop');
-      console.log('oldname: '+ oldName)
-      console.log('newname: '+ newName)
-      if (oldName === clientList[i]) {
-        clientList[i] = newName;
-        console.log('oldname: '+ oldName)
-        console.log('newname: '+ newName)
-        console.log('clientList: '+ clientList)
+      if (name === clientList[i]) {
+        console.log("name is: " + name);
+        clientList.splice(i, 1);
       };
     }
     updateClientUserList(clientList);
+    console.log('client list after update: ' + clientList);
   };
+
+  // Not in use - name change form disabled
+
+  // function updateServerUserList(oldName, newName, clientList) {
+  //   console.log('started server user list function');
+  //   console.log('client list: ' + clientList.toString());
+  //   for (var i = 0; i < clientList.length; i++) {
+  //     console.log('in for looop');
+  //     console.log('oldname: '+ oldName)
+  //     console.log('newname: '+ newName)
+  //     if (oldName === clientList[i]) {
+  //       clientList[i] = newName;
+  //       console.log('oldname: '+ oldName)
+  //       console.log('newname: '+ newName)
+  //       console.log('clientList: '+ clientList)
+  //     };
+  //   }
+  //   updateClientUserList(clientList);
+  // };
 
   function updateClientUserList(clientList){
     io.sockets.emit('sync user list', clientList);
