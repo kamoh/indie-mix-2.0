cf40429bbea89105ad156ffa76e2b71c697391b1
kamoh <ben.serviss@gmail.com>
2014-12-07 00:41:14 -0500

    Correctly updates all clients with the correct user list when users join

--- index.js
+++ index.js
@@ -13,28 +13,55 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   console.log('a user connected');
-  socket.on('disconnect', function(name){
-    io.emit('user left', name);
-    console.log(this.name + ' disconnected');
-  });
   socket.on('sign on', function(name){
+    allClients.push(name);
     io.emit('someone joined', name);
-    allClients.push(this);
+    updateClientUserList(allClients);
     console.log(name + ' joined the chat');
     console.log('current clients: ' + allClients.toString());
     });
-  socket.on('sync user list', function(listOfNames){
-    io.emit('sync user list', listOfNames);
-    console.log('syncing user names');
-  });
+  // socket.on('sync user list', function(listOfNames){
+  //   io.emit('sync user list', listOfNames);
+  //   console.log('syncing user names');
+  // });
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
     console.log('message: ' + msg);
   });
   socket.on('name change', function(defaultName, newName){
-    io.emit('name change', defaultName, newName);
+    // io.emit('name change', defaultName, newName);
+    updateServerUserList(defaultName, newName, allClients);
     console.log('roll call updated');
   });
+  socket.on('disconnect', function(name){
+    io.emit('user left', name);
+    console.log(this.name + ' disconnected');
+  });
+  
+  // Functions
+
+  function updateServerUserList(oldName, newName, clientList) {
+    console.log('started server user list function');
+    console.log('client list: ' + clientList.toString());
+    for (var i = 0; i < clientList.length; i++) {
+      console.log('in for looop');
+      console.log('oldname: '+ oldName)
+      console.log('newname: '+ newName)
+      if (oldName === clientList[i]) {
+        clientList[i] = newName;
+        console.log('oldname: '+ oldName)
+        console.log('newname: '+ newName)
+        console.log('clientList: '+ clientList)
+      };
+    }
+    updateClientUserList(clientList);
+  };
+
+  function updateClientUserList(clientList){
+    io.sockets.emit('sync user list', clientList);
+    console.log('currnet users: ' + clientList.toString());
+  };
+
 });
 
 http.listen(process.env.PORT || 3000, function(){
